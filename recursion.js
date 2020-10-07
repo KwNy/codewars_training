3ca3dd618156f3fa181e79181f140954aae4e4b1
KwNy <38081075+KwNy@users.noreply.github.com>
2020-10-07 14:57:07 +0200

    Update recursion.js

--- recursion.js
+++ recursion.js
@@ -16,3 +16,15 @@ function factorial(num){
     return num* factorial(num-1)
 }
 
+// productOfArray([1,2,3]) // 6
+// productOfArray([1,2,3,10]) // 60
+function productOfArray(arr){
+    
+    if(arr.length === 0){
+        return 1
+    }
+    
+   
+    
+    return arr[0]*productOfArray(arr.slice(1)) 
+}
