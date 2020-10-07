998837a7323bb9664b1944c1d8640d03e3cda6e7
KwNy <38081075+KwNy@users.noreply.github.com>
2020-10-07 15:18:05 +0200

    Update recursion.js

--- recursion.js
+++ recursion.js
@@ -47,3 +47,17 @@ function fib(num){
   return fib(num-1) +fib(num-2)
 }
 
+function reverse(str){
+  // add whatever parameters you deem necessary - good luck!
+  if(str.length === 1){
+      
+      return str
+      
+  }
+  
+  return reverse(str.slice(1))+str[0]
+  
+}
+
+// reverse('awesome') // 'emosewa'
+// reverse('rithmschool') // 'loohcsmhtir'
